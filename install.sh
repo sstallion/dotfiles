7c35854aa91c0d182c1158afca80131f06fb29c9
Steven Stallion <sstallion@gmail.com>
2020-06-02 11:14:18 -0500

    Add key bindings for less

--- install.sh
+++ install.sh
@@ -7,14 +7,17 @@ git submodule update --init --recursive
 # Link each file to the home directory iff the file does not exist or is
 # already a symbolic link. This will probably annoy someone, eventually.
 for file in `pwd`/files/*; do
-    link_name="$HOME/.`basename $file`"
-    if [ -e $link_name -a ! -h $link_name ]; then
-        echo "Skipping $link_name; not a symbolic link"
-    else
-        echo "Installing $link_name..."
-        ln -sfT $file $link_name
-    fi
+	link_name="$HOME/.`basename $file`"
+	if [ -e $link_name -a ! -h $link_name ]; then
+		echo "Skipping $link_name; not a symbolic link"
+	else
+		echo "Installing $link_name..."
+		ln -sfT $file $link_name
+	fi
 done
+
+echo "Updating lesskey file..."
+lesskey
 
 echo "Done."
 exit 0
