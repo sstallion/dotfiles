6000e37f2b1d95c1af65eedffc5736739a3d7bcf
Steven Stallion <sstallion@gmail.com>
2020-06-02 11:14:22 -0500

    Yet more adjustments for Linux

--- files/tmux/compat.sh
+++ files/tmux/compat.sh
@@ -2,34 +2,46 @@
 # compat.sh - tmux compatibility
 
 # tmux does a terrible job integrating with the underlying operating
-# system, especially when it is executing in a graphical environment. To
-# add insult to injury, backward compatibility is not maintained for
+# system, especially when it is executing in a graphical environment.
+# To add insult to injury, backward compatibility is not maintained for
 # options, which change meaning between versions. This script attempts
-# to bridge the divide so a common tmux.conf can be used on different
+# to bridge this divide so a common tmux.conf can be used on different
 # systems running different versions without issue.
 
 case `uname -s` in
 Darwin)
+	# Reattach to the per-user namespace to access the pasteboard;
+	# see: https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard.
 	tmux set-option -g default-command "reattach-to-user-namespace -l $SHELL"
-	tmux bind-key -t vi-copy Enter copy-pipe pbcopy
-	tmux bind-key -t vi-copy y     copy-pipe pbcopy
+
+	# Key Bindings
+	tmux bind-key -t vi-copy Enter copy-pipe "pbcopy"
+	tmux bind-key -t vi-copy y     copy-pipe "pbcopy"
 	tmux bind-key ] run-shell "pbpaste | tmux load-buffer - && tmux paste-buffer"
 	;;
 Linux)
-	tmux bind-key -t vi-copy Enter copy-pipe "xclip -i -selection clipboard"
-	tmux bind-key -t vi-copy y     copy-pipe "xclip -i -selection clipboard"
-	tmux bind-key ] run-shell "xclip -o | tmux load-buffer - && tmux paste-buffer"
+	# Disable terminal clipboard when using gnome-terminal;
+	# see: http://askubuntu.com/a/507215.
+	tmux set-option -g set-clipboard off
+
+	# Key Bindings
+	tmux bind-key -t vi-copy Enter copy-pipe "xsel -i"
+	tmux bind-key -t vi-copy y     copy-pipe "xsel -i"
+	tmux bind-key ] run-shell "xsel -o | tmux load-buffer - && tmux paste-buffer"
 	;;
 esac
 
 version=`tmux -V | cut -c 6-`
 has_version() {
-	return `echo $@ | awk "{print !($version >= \$1)}"`
+	return `echo $@ | awk "{print ($version >= \$1) == 0}"`
 }
 
 if has_version 2.1; then
+	# Enable mouse support:
 	tmux set-option -g mouse on
+
 elif has_version 1.6; then
+	# Enable mouse support:
 	tmux set-option -g mouse-mode on
 	tmux set-option -g mouse-resize-pane on
 	tmux set-option -g mouse-select-pane on
