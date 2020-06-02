b4749b204b2fd91d5c635deffa76d82bfda85b1f
Steven Stallion <sstallion@gmail.com>
2020-06-02 11:14:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ git submodule update --init --recursive
 
 # Link each file to the home directory iff the file does not exist or is
 # already a symbolic link. This will probably annoy someone, eventually.
-for file in `pwd`/files/*; do
-	link_name="$HOME/.`basename $file`"
+for target in `pwd`/files/*; do
+	link_name=$HOME/.`basename $target`
 	if [ -e $link_name -a ! -h $link_name ]; then
 		echo "Skipping $link_name; not a symbolic link"
 	else
 		echo "Installing $link_name..."
-		ln -sfT $file $link_name
+		ln -fs -T $target $link_name
 	fi
 done
 
