d3cfe5ead5c2182187cfa59ab9d9dfcb1eac3fec
Steven Stallion <sstallion@gmail.com>
2020-06-02 11:14:22 -0500

    Update permissions in install.sh

--- install.sh
+++ install.sh
@@ -4,20 +4,21 @@
 echo "Updating submodules..."
 git submodule update --init --recursive
 
+echo "Updating permissions..."
+chmod 0700 files/ssh
+chmod 0600 files/ssh/config
+
 # Link each file to the home directory iff the file does not exist or is
 # already a symbolic link. This will probably annoy someone, eventually.
-for target in `pwd`/files/*; do
+for target in files/*; do
 	link_name=$HOME/.`basename $target`
 	if [ -e $link_name -a ! -h $link_name ]; then
 		echo "Skipping $link_name; not a symbolic link"
 	else
 		echo "Installing $link_name..."
-		ln -fs -T $target $link_name
+		ln -fs -T $PWD/$target $link_name
 	fi
 done
-
-echo "Updating permissions..."
-chmod 0700 $HOME/.ssh/
 
 echo "Updating lesskey file..."
 lesskey
