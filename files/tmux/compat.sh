e68fb2158e144a77e146e034ecc6abb52cf49610
Steven Stallion <steven.stallion@sony.com>
2024-02-08 10:59:52 -0800

    Modernize tmux.conf

--- files/tmux/compat.sh
+++ files/tmux/compat.sh
@@ -1,5 +1,7 @@
-#!/bin/sh -x
+#!/bin/sh
 # compat.sh - tmux compatibility
+
+set -o xtrace
 
 # tmux does a terrible job integrating with the underlying operating
 # system, especially when it is executing in a graphical environment.
@@ -9,36 +11,32 @@
 # systems running different versions without issue.
 
 has_version() {
-	[ -z "$version" ] && version=$(tmux -V | cut -c 6-)
-	return $(echo $@ | awk "{print (($version >= \$1) == 0)}")
+	[ -z "${version}" ] && version=$(tmux -V | cut -c 6-)
+	return $(echo $@ | awk "{print ((${version} >= \$1) == 0)}")
 }
 
 is_remote() {
-	test -n "$SSH_CONNECTION"
+	test -n "${SSH_CONNECTION}"
 }
 
-case $(uname -s) in
+case "$(uname -s)" in
 Darwin)
 	# Reattach to the per-user namespace to access the pasteboard.
 	# For macOS Sierra, specify `--with-wrap-pbcopy-and-pbpaste`
 	# when installing reattach-to-user-namespace via brew(1);
 	# see: https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard,
 	#      http://superuser.com/a/413233.
-	tmux set-option -g default-command "reattach-to-user-namespace -l $SHELL"
+	tmux set-option -g default-command "reattach-to-user-namespace -l ${SHELL}"
 
 	# Key Bindings
-	if has_version 2.4; then
-		tmux bind-key -T copy-mode-vi Enter send-keys -X copy-pipe-and-cancel "pbcopy"
-		tmux bind-key -T copy-mode-vi y     send-keys -X copy-pipe-and-cancel "pbcopy"
-	else
-		tmux bind-key -t vi-copy Enter copy-pipe "pbcopy"
-		tmux bind-key -t vi-copy y     copy-pipe "pbcopy"
-	fi
+	tmux bind-key -T copy-mode-vi Enter             send-keys -X copy-pipe-and-cancel "pbcopy"
+	tmux bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-no-clear   "pbcopy"
+	tmux bind-key -T copy-mode-vi y                 send-keys -X copy-pipe-and-cancel "pbcopy"
 	tmux bind-key ] run-shell "pbpaste | tmux load-buffer - && tmux paste-buffer"
 	;;
 *)
 	# Disable terminal clipboard when using gnome-terminal;
-	# see: http://askubuntu.com/a/507215.
+	# see: https://github.com/tmux/tmux/wiki/Clipboard#terminal-support---vte-terminals.
 	tmux set-option -g set-clipboard off
 
 	# Experimental support for (unidirectional) clipboard sharing;
@@ -52,16 +50,12 @@ Darwin)
 	fi
 
 	# Key Bindings
-	if has_version 2.4; then
-		tmux bind-key -T copy-mode-vi Enter send-keys -X copy-pipe-and-cancel "$copy_command"
-		tmux bind-key -T copy-mode-vi y     send-keys -X copy-pipe-and-cancel "$copy_command"
-	else
-		tmux bind-key -t vi-copy Enter copy-pipe "$copy_command"
-		tmux bind-key -t vi-copy y     copy-pipe "$copy_command"
-	fi
+	tmux bind-key -T copy-mode-vi Enter             send-keys -X copy-pipe-and-cancel "${copy_command}"
+	tmux bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-no-clear   "${copy_command}"
+	tmux bind-key -T copy-mode-vi y                 send-keys -X copy-pipe-and-cancel "${copy_command}"
 
-	[ -n "$paste_command" ] &&
-		tmux bind-key ] run-shell "$paste_command | tmux load-buffer - && tmux paste-buffer"
+	[ -n "${paste_command}" ] &&
+		tmux bind-key ] run-shell "${paste_command} | tmux load-buffer - && tmux paste-buffer"
 	;;
 esac
 
@@ -73,32 +67,16 @@ else
 fi
 
 # Key Bindings
-if has_version 2.4; then
-	tmux bind-key -T copy-mode-vi C-j    send-keys -X page-down
-	tmux bind-key -T copy-mode-vi C-k    send-keys -X page-up
+tmux bind-key -T copy-mode-vi C-j    send-keys -X page-down
+tmux bind-key -T copy-mode-vi C-k    send-keys -X page-up
 
-	tmux bind-key -T copy-mode-vi Escape send-keys -X cancel
-	tmux bind-key -T copy-mode-vi v      send-keys -X begin-selection
-else
-	tmux bind-key -t vi-copy      C-j    page-down
-	tmux bind-key -t vi-copy      C-k    page-up
-
-	tmux bind-key -t vi-copy      Escape cancel
-	tmux bind-key -t vi-copy      v      begin-selection
-fi
+tmux bind-key -T copy-mode-vi Escape send-keys -X cancel
+tmux bind-key -T copy-mode-vi v      send-keys -X begin-selection
 
 # Focus Support
-if has_version 1.9; then
-	tmux set-option -g focus-events on
-fi
+tmux set-option -g focus-events on
 
 # Mouse Support
-if has_version 2.1; then
-	tmux set-option -g mouse on
-elif has_version 1.6; then
-	tmux set-option -g mouse-mode on
-	tmux set-option -g mouse-resize-pane on
-	tmux set-option -g mouse-select-pane on
-	tmux set-option -g mouse-select-window on
-fi
+tmux set-option -g mouse on
+tmux unbind-key -n MouseDrag1Border
 exit 0
