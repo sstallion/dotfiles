ea65ec9d72c853bd57a2939da045e075b4cef160
Steven Stallion <sstallion@gmail.com>
2024-04-05 13:46:47 -0500

    Update Windows support

--- extra/Windows/Programs/start-gpg-agent.bat
+++ extra/Windows/Programs/start-gpg-agent.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
 ECHO Starting GnuPG Agent...
-"%ProgramFiles(x86)%\gnupg\bin\gpg-connect-agent.exe" killagent /bye
+"%ProgramFiles(x86)%\gnupg\bin\gpg-connect-agent.exe" killagent /bye 2>NUL
 "%ProgramFiles(x86)%\gnupg\bin\gpg-connect-agent.exe" /bye
 TIMEOUT /T 5 /NOBREAK >NUL
 "%ProgramFiles(x86)%\gnupg\bin\gpg.exe" --card-status
+TIMEOUT /T 1 /NOBREAK >NUL
+IF %ERRORLEVEL% NEQ 0 PAUSE
